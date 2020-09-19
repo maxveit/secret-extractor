7e2f064ddc94870603a1eb0da316d771095e00f3
Max Veit <max.veit1@de.ibm.com>
2020-09-19 16:39:59 +0200

    switched to parameterized script to handle arbitrary secrets

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-set -x
 
 ENV_NAME=QUARKUS_REDIS_PASSWORD
-SECRET=`cat /usr/connection/connection | jq .rediss.authentication.password | tr -d '"'`
 
-cp /usr/.env /tmp/.env
-sed -i 's/ENV_NAME/'"$ENV_NAME"'/g' /tmp/.env
-sed -i 's/SECRET/'"$SECRET"'/g' /tmp/.env
+echo "ENV_NAME=$ENV_NAME"
+echo "MOUNT_PATH=$MOUNT_PATH"
+echo "SECRET_PATH=$SECRET_PATH"
 
-touch /password/test
-cp /tmp/.env /password/.env
+SECRET=`cat $MOUNT_PATH | jq $SECRET_PATH | tr -d '"'`
 
-cat /password/.env
+echo "$ENV_NAME=$SECRET" >> /password/.env
 
 ls -la /password/
