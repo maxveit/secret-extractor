ef73dcb76d20cc3105c763324e72101196387149
Max Veit <max.veit1@de.ibm.com>
2020-09-19 16:45:09 +0200

    further parameterized

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-ENV_NAME=QUARKUS_REDIS_PASSWORD
-
 echo "ENV_NAME=$ENV_NAME"
 echo "MOUNT_PATH=$MOUNT_PATH"
 echo "SECRET_PATH=$SECRET_PATH"
 
 SECRET=`cat $MOUNT_PATH | jq $SECRET_PATH | tr -d '"'`
 
-echo "$ENV_NAME=$SECRET" >> /password/.env
+echo "$ENV_NAME=$SECRET" >> /secrets/env_list
 
-ls -la /password/
+ls -la /secrets/
+wc -l /secrets/env_list
