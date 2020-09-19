7202535171cd81a6ff9271d432ca5cd328fa70ed
Max Veit <max.veit1@de.ibm.com>
2020-09-19 15:15:44 +0200

    removed debug

--- run.sh
+++ run.sh
@@ -2,20 +2,13 @@
 set -x
 
 ENV_NAME=QUARKUS_REDIS_PASSWORD
-PASSWORD=`cat /usr/connection/connection | jq .rediss.authentication.password | tr -d '"'`
+SECRET=`cat /usr/connection/connection | jq .rediss.authentication.password | tr -d '"'`
 
 cp /usr/.env /tmp/.env
 sed -i 's/ENV_NAME/'"$ENV_NAME"'/g' /tmp/.env
-sed -i 's/PASSWORD/'"$PASSWORD"'/g' /tmp/.env
-
-cat /tmp/.env
+sed -i 's/SECRET/'"$SECRET"'/g' /tmp/.env
 
 touch /password/test
-echo "debug" > /password/test
-cat  /password/test
 cp /tmp/.env /password/.env
 
 cat /password/.env
-
-ls -la /
-ls -la /password/
